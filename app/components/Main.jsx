1bad0c753e6bdb2e1cfdc3af46b3872ecfb84378
Alejjaandro <alejandroolaso98@gmail.com>
2023-11-02 09:20:59 +0100

    styles changes and email sending

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -38,9 +38,11 @@ export default function Main() {
                             </div>
                         </a>
 
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <AiOutlineMail />
-                        </div>
+                        <a href="/#sendEmail">
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <AiOutlineMail />
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,8 +17,8 @@ export default function Navbar() {
     const path = usePathname();
 
     useEffect(() => {
-        if(
-            path === '/ecommerce' || 
+        if (
+            path === '/ecommerce' ||
             path === '/upcomings'
         ) {
             setNavBg('transparent');
@@ -31,7 +31,7 @@ export default function Navbar() {
 
     useEffect(() => {
         const handleShadow = () => {
-            if (window.scrollY >= 90) {setShadow(true)} else {setShadow(false)}
+            if (window.scrollY >= 90) { setShadow(true) } else { setShadow(false) }
         };
 
         window.addEventListener('scroll', handleShadow);
@@ -40,28 +40,25 @@ export default function Navbar() {
     const handleSideNavbar = () => setSideNavbar(!sideNavbar)
 
     return (
-        <div style={{backgroundColor: `${navBg}`}} className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
-            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-                <Image src={navLogo} alt="/" width={70} height={0} />
-                <div>
-                    <ul style={{color: `${linkColor}`}} className="hidden md:flex">
-                        <Link href='/'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-                        </Link>
-                        <Link href='/#about'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">About</li>
-                        </Link>
-                        <Link href='/#skills'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
-                        </Link>
-                        <Link href='/#projects'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
-                        </Link>
-                        <Link href='/#contact'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
-                        </Link>
-                    </ul>
-                </div>
+        <div style={{ backgroundColor: `${navBg}` }} className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
+            <div className="flex justify-around items-center w-full h-full px-2 2xl:px-16">
+                <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
+                    <Link href='/'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+                    </Link>
+                    <Link href='/#about'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+                    </Link>
+                    <Link href='/#skills'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+                    </Link>
+                    <Link href='/#projects'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+                    </Link>
+                    <Link href='/#contact'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+                    </Link>
+                </ul>
                 <div onClick={handleSideNavbar} className="md:hidden">
                     <AiOutlineMenu size={25} />
                 </div>
@@ -74,14 +71,14 @@ export default function Navbar() {
                         ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500"
                         : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
                 }>
-                    <div className="flex w-full items-center justify-between">
-                        <Image src={navLogo} alt="/" width={70} height={0} />
+                    <div className="flex w-full items-center justify-end">
                         <div onClick={handleSideNavbar} className="rounded-full shadow-lg shadow-gray-400 cursor-pointer p-4">
                             <AiOutlineClose size={25} />
                         </div>
                     </div>
 
                     <div className="border-b border-gray-300 my-4">
+                        <h3 className="py-2 text-xl">Alejandro Olaso</h3>
                         <p className="py-2">Full Stack Developer</p>
                     </div>
 
@@ -107,20 +104,22 @@ export default function Navbar() {
                         <div className="pt-40">
                             <p className="uppercase tracking-widest text-violet-600 mb-4">{"Let's Connect"}</p>
                             <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-                            <a href='https://www.linkedin.com/in/alejandro-olaso-fullstackweb/' target='_blank' rel='noreferrer'>
-                                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-150'>
-                                            <FaLinkedinIn />
-                                        </div>
-                                    </a>
-                                    <a href='https://github.com/Alejjaandro' target='_blank' rel='noreferrer'>
-                                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-150'>
-                                            <FaGithub />
-                                        </div>
-                                    </a>
+                                <a href='https://www.linkedin.com/in/alejandro-olaso-fullstackweb/' target='_blank' rel='noreferrer'>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-150'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                </a>
+                                <a href='https://github.com/Alejjaandro' target='_blank' rel='noreferrer'>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-150'>
+                                        <FaGithub />
+                                    </div>
+                                </a>
 
+                                <a href="/#sendEmail">
                                     <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-150'>
                                         <AiOutlineMail />
                                     </div>
+                                </a>
                             </div>
                         </div>
                     </div>
