801ae85b427379a0ca39591fa8fb5bccaa55ac2a
Alejjaandro <alejandroolaso98@gmail.com>
2025-03-11 09:57:50 +0100

    image fix

--- app/companyWeb/page.jsx
+++ app/companyWeb/page.jsx
@@ -11,7 +11,7 @@ export default function ecommerce() {
         <div className='w-full'>
             <div className='w-screen h-[50vh] relative'>
                 <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
-                <Image src={`/projects/companyWeb.png`} className='absolute z-1' layout='fill' objectFit='cover' alt='/' />
+                <Image src={CompanyWebImg} className='absolute z-1' layout='fill' objectFit='cover' alt='/' />
                 <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
                     <h2 className='py-2'>Company Web</h2>
                     <h3>Next JS / Tailwind CSS</h3>
